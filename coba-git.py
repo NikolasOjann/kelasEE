49280ff9b2bb2920497630188f3b2f428f06f56d
mochammad ilham fauzan <mochammadilhamfau078@gmail.com>
2024-11-29 10:12:03 +0700

    ini versi 2

--- coba-git.py
+++ coba-git.py
@@ -1,21 +1,2 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "print(\"Aku ojan\")\n",
-    "\n"
-   ]
-  }
- ],
- "metadata": {
-  "language_info": {
-   "name": "python"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+print("jansen")
+print("ilham fauzan")
